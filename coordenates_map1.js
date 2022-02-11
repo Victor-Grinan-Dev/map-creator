c2748427a899cb108e7d4e67e61e9e2ffadf670a
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-02-11 20:20:20 +0200

    lots of stuff

--- coordenates_map1.js
+++ coordenates_map1.js
@@ -4,60 +4,52 @@ const rows = [60, 250, 430, 620, 800]
 
 //left values per rows odd & pair
 const oddRowColumns = [50, 260, 470, 680, 890, 1100, 1320, 1530]
-const pairRowColumns = [160, 370, 580, 790, 1000, 1210, 1420]
+const pairRowColums = [160, 370, 580, 790, 1000, 1210, 1420]
 
 //position name [top,left] value in px, add 210 to the left
-const pos = {
-    a1:[rows[0], oddRowColumns[0], ["b1", "b2"]],
-    b1:[rows[0], oddRowColumns[1], ["a1", "b2", "c2", "c1"]],
-    c1:[rows[0], oddRowColumns[2], ["b1", "c2", "d2", "d1"]],
-    d1:[rows[0], oddRowColumns[3], ["c1", "d2", "e2", "e1"]],
-    e1:[rows[0], oddRowColumns[4], ["d1", "e2", "f2", "f1"]],
-    f1:[rows[0], oddRowColumns[5], ["e1", "f2", "g2", "g1"]],
-    g1:[rows[0], oddRowColumns[6], ["f1", "g2", "h2", "h1"]],
-    h1:[rows[0], oddRowColumns[7], ["g1"], "h2"],
+const mapGrid = {
+    a1:[rows[0], oddRowColumns[0]],
+    b1:[rows[0], oddRowColumns[1]],
+    c1:[rows[0], oddRowColumns[2]],
+    d1:[rows[0], oddRowColumns[3]],
+    e1:[rows[0], oddRowColumns[4]],
+    f1:[rows[0], oddRowColumns[5]],
+    g1:[rows[0], oddRowColumns[6]],
+    h1:[rows[0], oddRowColumns[7]],
 
-    b2:[rows[1], pairRowColumns[0], ["a1", "b1", "c2", "c3", "b3"]],
-    c2:[rows[1], pairRowColumns[1], ["b1", "c1", "d2", "d3", "c3", "b2"]],
-    d2:[rows[1], pairRowColumns[2], ["c1", "d1", "e2", "e3", "d3", "c2"]],
-    e2:[rows[1], pairRowColumns[3], []],
-    f2:[rows[1], pairRowColumns[4], []],
-    g2:[rows[1], pairRowColumns[5], []],
-    h2:[rows[1], pairRowColumns[6], []],
+    b2:[rows[1], pairRowColumns[0]],
+    c2:[rows[1], pairRowColumns[1]],
+    d2:[rows[1], pairRowColumns[2]],
+    e2:[rows[1], pairRowColumns[3]],
+    f2:[rows[1], pairRowColumns[4]],
+    g2:[rows[1], pairRowColumns[5]],
+    h2:[rows[1], pairRowColumns[6]],
 
-    b3:[rows[2], oddRowColumns[0], []],
-    c3:[rows[2], oddRowColumns[1], []],
-    d3:[rows[2], oddRowColumns[2], []],
-    e3:[rows[2], oddRowColumns[3], []],
-    f3:[rows[2], oddRowColumns[4], []],
-    g3:[rows[2], oddRowColumns[5], []],
-    h3:[rows[2], oddRowColumns[6], []],
-    i3:[rows[2], oddRowColumns[7], []],
+    b3:[rows[2], oddRowColumns[0]],
+    c3:[rows[2], oddRowColumns[1]],
+    d3:[rows[2], oddRowColumns[2]],
+    e3:[rows[2], oddRowColumns[3]],
+    f3:[rows[2], oddRowColumns[4]],
+    g3:[rows[2], oddRowColumns[5]],
+    h3:[rows[2], oddRowColumns[6]],
+    i3:[rows[2], oddRowColumns[7]],
 
-    c4:[rows[3], pairRowColumns[0], []],
-    d4:[rows[3], pairRowColumns[1], []],
-    e4:[rows[3], pairRowColumns[2], []],
-    f4:[rows[3], pairRowColumns[3], []],
-    g4:[rows[3], pairRowColumns[4], []],
-    h4:[rows[3], pairRowColumns[5], []],
-    i4:[rows[3], pairRowColumns[6], []],
+    c4:[rows[3], pairRowColumns[0]],
+    d4:[rows[3], pairRowColumns[1]],
+    e4:[rows[3], pairRowColumns[2]],
+    f4:[rows[3], pairRowColumns[3]],
+    g4:[rows[3], pairRowColumns[4]],
+    h4:[rows[3], pairRowColumns[5]],
+    i4:[rows[3], pairRowColumns[6]],
 
-    c5:[rows[4], oddRowColumns[0], []],
-    d5:[rows[4], oddRowColumns[1], []],
-    e5:[rows[4], oddRowColumns[2], []],
-    f5:[rows[4], oddRowColumns[3], []],
-    g5:[rows[4], oddRowColumns[4], []],
-    h5:[rows[4], oddRowColumns[5], []],
-    i5:[rows[4], oddRowColumns[6], []],
-    j5:[rows[4], oddRowColumns[7], []],
+    c5:[rows[4], oddRowColumns[0]],
+    d5:[rows[4], oddRowColumns[1]],
+    e5:[rows[4], oddRowColumns[2]],
+    f5:[rows[4], oddRowColumns[3]],
+    g5:[rows[4], oddRowColumns[4]],
+    h5:[rows[4], oddRowColumns[5]],
+    i5:[rows[4], oddRowColumns[6]],
+    j5:[rows[4], oddRowColumns[7]],
 }
 
-
-const reposition = (item, xy) => {
-
-    console.log(item, `top: ${xy[0]}px, bottom: ${xy[1]}px`)
-    
-    document.querySelector(item).style.top = `${xy[0]}px`;
-    document.querySelector(item).style.left = `${xy[1]}px`;
-    //document.div.style.backgroundImage = `linear-gradient(${selectedDirection}, ${color1.value}, ${color2.value})`
-}
+document.querySelector("")
--- objects.js
+++ objects.js
@@ -1,28 +1,49 @@
 //tokens are the chips that represent in the map the differents formations wich your army is divided
-const token = {
-    named:"unknown",
-    fieldTask:"unknown",
-    faction: "unknown", //related to the army it belongs
-    color: "unknown", //this should be the army-wide color
-    strenght: 0, //is the points values of the formations. if strenght = 0: remove formation.
-    stats: {movement:2, vision:1}, 
-    upgrades: [], //all special rules and modifiers gained after game started
-    level: 0,
-    currentPosition: "a1", //a1 is a tile, tiles are coordenates (x, y) of the entire map
-} 
 
-const army = {
-    named:"unknown",
-    color:"unknown",
-    campaingPoints: 0,
-    formations:[], //a list of objects tokens that compose the army
-    overallStrength: 0,
+class Formation {
+
+    constructor (named, fieldTask, faction, color, strenght, stats, upgrades, level, currentPosition){
+        this.named = named;            
+        this.fieldTask = fieldTask;        
+        this.faction = faction;        
+        this.color = color;        
+        this.strenght = strenght;        
+        this.stats = stats;        
+        this.upgrades = upgrades;        
+        this.level = level;        
+        this.currentPosition = currentPosition; 
+        this.actions = 2;
+    }
 }
 
-const user = {
-    named:"unknown",
-    email:"unknown",
-    armies:[]
+class Army {
+
+    constructor (named, color, campaingPoints, formations, overallStrength){
+        this.named = named;
+        this.color = color;   
+        this.campaingPoints = campaingPoints;        
+        this.formations = formations;    //a list of objects tokens that compose the army 
+        this.overallStrength = overallStrength;                 
+    }
+}
+
+class User {
+
+    constructor (named, email, armies){
+        this.named = named;
+        this.email = email;
+        this.armies = armies; //array of all armies
+    }
+}
+
+
+class map {
+
+    constructor (named, image, coordinates){
+        this.named = named;
+        this.image = image; //url
+        this.coords = coordinates; //dictionary of all the top.left px of the image
+    }
 }
 
 const map = {
