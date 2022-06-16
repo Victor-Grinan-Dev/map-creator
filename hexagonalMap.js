eae8095e1f7f042280a7ae8974df0f845dd63da2
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-06-16 14:44:43 +0300

    now is mega ready!!!

--- hexagonalMap.js
+++ hexagonalMap.js
@@ -14,7 +14,7 @@ const diagonChar = [
     'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
-const HexagonalMap = (side = 6) => {
+const HexagonalMap = (side = 9) => {
     
     let tile = ""
     const width = side * 3 - 2;
@@ -60,3 +60,4 @@ const HexagonalMap = (side = 6) => {
 
 const hexMap = HexagonalMap()
 
+console.table(hexMap);
